ec244435bb8ff7cabacfbd65bc5fbe7090946d73
Zhao Tang <tangzhao1999@hotmail.com>
2023-02-11 15:08:19 -0800

    added com prediction package

--- ros_tensorflow/setup.py
+++ ros_tensorflow/setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['contact_graspnet'],
+    packages=['contact_graspnet', 'CoM_prediction'],
     package_dir={'': 'src'}
     )
 setup(**setup_args)
