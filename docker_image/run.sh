fc54e6ee50fe52d2915a4f75754592babf0d24d0
jiaming hu <jih189@ucsd.edu>
2023-03-13 00:17:20 -0700

    init docker setup

---
+++ docker_image/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+docker run  -v $PWD/shared:/shared -e DISPLAY=":0" \
+    -e QT_X11_NO_MITSHM=1 \
+	-e XAUTHORITY \
+	-e NVIDIA_DRIVER_CAPABILITIES=all \
+	--ipc=host \
+	--gpus all \
+	--net=host \
+    	--privileged=true \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" -p 19997:19997 -it coppeliasim-ubuntu18 "$@"
