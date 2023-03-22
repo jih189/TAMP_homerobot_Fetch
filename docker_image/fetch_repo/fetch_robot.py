5ca82b012c24c50db07e99c8984980a82bdcaf6b
jiaming hu <jih189@ucsd.edu>
2023-03-22 00:14:12 -0700

    get joint names

--- docker_image/fetch_repo/fetch_robot.py
+++ docker_image/fetch_repo/fetch_robot.py
@@ -3,14 +3,21 @@ import sys
 sys.path.append('/root/zmqRemoteApi/clients/python')
 from zmqRemoteApi import RemoteAPIClient
 
-# create a client to connect to zmqRemoteApi server:
-# (creation arguments can specify different host/port,
-# defaults are host='localhost', port=23000)
-client = RemoteAPIClient()
+class FetchRobot:
+    def __init__(self):
+        self.client = RemoteAPIClient()
+        self.sim = self.client.getObject('sim')
+        self.name = 'fetch'
+        self.robot_object = self.sim.getObject('/'+self.name+'_respondable')
 
-# get a remote object:
-sim = client.getObject('sim')
+    def getJointNames(self):
+        result = [self.sim.getObjectAlias(i) for i in self.sim.getObjectsInTree(self.robot_object, self.sim.object_joint_type)]
+        return result
 
-# call API function:
-h = sim.getObject('/fetch_respondable')
-print(h)
+
+def main():
+    fetch_robot = FetchRobot()
+    print(fetch_robot.getJointNames())
+
+if __name__ == "__main__":
+    main()
