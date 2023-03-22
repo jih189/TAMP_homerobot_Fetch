f70f427fdd0133051121bb6318a7662455562d67
jih189 <jih189@ucsd.edu>
2023-03-22 10:40:31 -0700

    before merge

--- docker_image/simple_run.sh
+++ docker_image/simple_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-docker run  -v $PWD/shared:/shared -e DISPLAY=":0" \
+docker run  -v $PWD/shared:/shared -e DISPLAY=":1" \
     -e QT_X11_NO_MITSHM=1 \
 	-e XAUTHORITY \
 	-e NVIDIA_DRIVER_CAPABILITIES=all \
