3955ff69c809c78788bb7f64319db7fcb110bb8b
jih189 <jih189@ucsd.edu>
2023-05-08 16:28:12 -0700

    include DL cbirrt

--- data_generation/script/deep_learning_based_test.py
+++ data_generation/script/deep_learning_based_test.py
@@ -23,7 +23,7 @@ def main():
     total_length_deep_learning = 0.0
     total_length_non_deep_learning = 0.0
 
-    test_number = 1
+    test_number = 100
 
     for i in range(test_number):
         env_num = i
@@ -33,46 +33,32 @@ def main():
         # publish the obstacle point cloud
         path_planner_tester.show_point_cloud(pointcloud)
 
-        hasTask, start_joint, target_joint = path_planner_tester.getRandomTask()
+        hasTask, start_joint, target_joint, task_constraints = path_planner_tester.getRandomTaskWithConstraints()
 
-        if not hasTask:
-            continue
+        # if not hasTask:
+        #     continue
 
         print("env ", env_num)
         print("start joint ", start_joint)
         print("target joint ", target_joint)
+        # print("constraints")
+        # print(task_constraints)
 
-        path_planner_tester.set_path_planner_id('DLBIRRTConfigDefault')
-        success, planning_time, path_length = path_planner_tester.measurePlanning(start_joint, target_joint)
+        # path_planner_tester.set_path_planner_id('DLBIRRTConfigDefault')
+        path_planner_tester.set_path_planner_id('CBIRRTConfigDefault')
+        success, planning_time, path_length = path_planner_tester.measurePlanningWithConstraints(start_joint, target_joint, task_constraints)
         if success:
             success_time_deep_learning += 1
             total_time_deep_learning += planning_time
             total_length_deep_learning += path_length
 
-        path_planner_tester.set_path_planner_id('RRTConnectkConfigDefault')
-        success, planning_time, path_length = path_planner_tester.measurePlanning(start_joint, target_joint)
+        # path_planner_tester.set_path_planner_id('RRTConnectkConfigDefault')
+        path_planner_tester.set_path_planner_id('DLCBIRRTConfigDefault')
+        success, planning_time, path_length = path_planner_tester.measurePlanningWithConstraints(start_joint, target_joint, task_constraints)
         if success:
             success_time_non_deep_learning += 1
             total_time_non_deep_learning += planning_time
             total_length_non_deep_learning += path_length
-
-
-        # # randonly generate two valid points and path the solution between them
-        # plan_result, path_length = path_planner_tester.motionTaskPlanning()
-        # if plan_result:
-        #     total_time += path_planner_tester.get_planning_time()
-        #     success_time += 1
-        #     print("in env ", env_num, " the planning time: ", path_planner_tester.get_planning_time())
-        #     print("path length ", path_length)
-        # else:
-        #     print("find no solution in ", env_num)
-        # path_planner_tester.print_task()
-
-        # # publish the pointcloud of the obstacle
-        # rate = rospy.Rate(20)
-        # while not rospy.is_shutdown():
-        #     path_planner_tester.show_point_cloud(pointcloud)
-        #     rate.sleep()
 
         # clear the planning scene.
         path_planner_tester.cleanPlanningScene()
