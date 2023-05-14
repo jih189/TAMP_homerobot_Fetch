8a86126b96c9b18f61717a244f28cfc2295613ff
jiaming hu <jih189@ucsd.edu>
2023-05-13 18:01:37 -0700

    include libtorch

--- docker_image/run.sh
+++ docker_image/run.sh
@@ -11,4 +11,5 @@ docker run -v $PWD/../:/root/catkin_ws/src/jiaming_manipulation \
 	-p 8888:8888 \
 	--privileged=true \
 	-v /etc/localtime:/etc/localtime:ro \
+	-v /usr/local/cuda:/usr/local/cuda \
 	-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" -p 19997:19997 -it jiaming-ubuntu18 bash
