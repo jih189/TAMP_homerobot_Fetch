d3fdefb81ec0d17167459a51c3c381aaad5cc9b3
jiaming hu <jih189@ucsd.edu>
2023-05-19 13:55:50 -0700

    include tensorboard

--- docker_image/run.sh
+++ docker_image/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 docker run -v $PWD/../:/root/catkin_ws/src/jiaming_manipulation \
-	-e DISPLAY=":1" \
+	-e DISPLAY=":0" \
 	-e QT_X11_NO_MITSHM=1 \
 	-e XAUTHORITY \
 	-e NVIDIA_DRIVER_CAPABILITIES=all \
@@ -9,6 +9,7 @@ docker run -v $PWD/../:/root/catkin_ws/src/jiaming_manipulation \
 	--gpus all \
 	--network="host" \
 	-p 8888:8888 \
+	-p 6006:6006 \
 	--privileged=true \
 	-v /etc/localtime:/etc/localtime:ro \
 	-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" -p 19997:19997 -it jiaming-ubuntu18 bash
