5831ba66f1a3d61dfb0ad34f5522536a2b602a14
jih189 <jih189@ucsd.edu>
2023-06-14 11:56:00 -0700

    first clean in contact grasp net

--- ros_tensorflow/src/contact_graspnet/contact_graspnet/model.py
+++ ros_tensorflow/src/contact_graspnet/contact_graspnet/model.py
@@ -12,7 +12,7 @@ import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-#tf.config.experimental.set_memory_growth(physical_devices[1], True)
+tf.config.experimental.set_memory_growth(physical_devices[1], True)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(BASE_DIR))
