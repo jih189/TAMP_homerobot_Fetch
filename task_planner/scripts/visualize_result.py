63d8fc9c58339685c79388f492c8a327ade1cc16
jih189 <jih189@ucsd.edu>
2023-08-18 13:04:35 -0700

    add visualization code for result

---
+++ task_planner/scripts/visualize_result.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import json
+import rospkg
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    '''
+    load the json file and visualize the result
+    '''
+
+    task_planner_names = ['MTGTaskPlanner', 'MDPTaskPlanner', 'MTGTaskPlannerWithGMM', 'MDPTaskPlannerWithGMM']
+
+    ##################################################################
+
+    rospack = rospkg.RosPack()
+    # Get the path of the desired package
+    package_path = rospack.get_path('task_planner')
+
+    result_json_path = package_path + '/evaluated_data_dir/evaluated_data.json'
+
+    with open(result_json_path) as f:
+        data = json.load(f)
+
+    total_time = [0.0 for i in range(len(task_planner_names))]
+    total_distance = [0.0 for i in range(len(task_planner_names))]
+    experiment_count = [0.0 for i in range(len(task_planner_names))]
+    success_count = [0.0 for i in range(len(task_planner_names))]
+
+    for i in range(len(data)):
+        for j in range(len(task_planner_names)):
+            if data[i]['task_planner'] == task_planner_names[j]:
+                experiment_count[j] += 1
+                if data[i]['found_solution']:
+                    success_count[j] += 1
+                    total_time[j] += data[i]['time']
+                    total_distance[j] += data[i]['total_distance']
+
+    average_time = [0.0 for i in range(len(task_planner_names))]
+    average_distance = [0.0 for i in range(len(task_planner_names))]
+    success_rate = [0.0 for i in range(len(task_planner_names))]
+
+    for i in range(len(task_planner_names)):
+        average_time[i] = total_time[i] / success_count[i]
+        average_distance[i] = total_distance[i] / success_count[i]
+        success_rate[i] = success_count[i] / experiment_count[i]
+
+    # visulize the average time, average distance, and success rate for each planner in plt
+    plt.figure(1)
+    plt.subplot(311)
+    plt.bar(task_planner_names, average_time)
+    plt.ylabel('Average Time')
+    plt.subplot(312)
+    plt.bar(task_planner_names, average_distance)
+    plt.ylabel('Average Distance')
+    plt.subplot(313)
+    plt.bar(task_planner_names, success_rate)
+    plt.ylabel('Success Rate')
+    plt.show()
